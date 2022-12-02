1ae12213b8dec0cc6d2ff0858c231e7174a359bc
Walber Oliveira <walberjr.oliveira@gmail.com>
2022-12-02 12:31:50 -0300

    #11 - Implementado tela de login. Faltam testes e a integração com a api

---
+++ src/components/buttons/ButtonSuccess.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+VBtn(
+  color="success"
+  @click="onClick"
+  block
+  elevation=2
+) {{ label }}
+</template>
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'BtnSuccess',
+  props: {
+    label: {
+      type: String,
+      default: 'OK',
+    },
+  },
+  methods: {
+    onClick() {
+      this.$emit('click');
+    },
+  },
+});
+</script>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,25 +1,39 @@
 <template lang="pug">
-VContainer
-  VCol
-    .text-h3.text-center LOGIN
-  VCol
-    VForm
-      InputEmail(v-model="user.username" label="Email")
-      InputPassword(v-model="user.password" label="Password")
-      VBtn(@click="onClick") Salvar
+.login-view
+  VImg(:src="require('@/assets/clash-1.jpg')" height="50vh")
+  VContainer.login-view_content
+    VRow(justify="center")
+      VCol(cols=4)
+        VCard(elevation=4 rounded)
+          .display-1.font-weight-light.text-center.pt-5 Bem vindo ao Clashe
+          VCardText
+            VForm(ref="loginForm" lazy-validation)
+              VRow
+                VCol(cols=12)
+                  InputEmail(v-model="user.username" label="Email" required)
+                VCol(cols=12)
+                  InputPassword(v-model="user.password" label="Password" required)
+                VCol(cols=12)
+                  ButtonSuccess(@click="onClick" label="Login")
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import InputEmail from '@/components/inputs/InputEmail.vue';
 import InputPassword from '@/components/inputs/InputPassword.vue';
+import ButtonSuccess from '@/components/buttons/ButtonSuccess.vue';
 import SummonerLogin from '@/domains/SummonerLogin';
+
+interface FormValidation extends Vue {
+  validate(): boolean
+}
 
 export default Vue.extend({
   name: 'LoginView',
   components: {
     InputEmail,
     InputPassword,
+    ButtonSuccess,
   },
   data(): { user: SummonerLogin } {
     return {
@@ -31,8 +45,17 @@ export default Vue.extend({
   },
   methods: {
     onClick() {
-      this.$router.push({ name: 'Team' });
+      const form = this.$refs.loginForm as FormValidation;
+      if (form.validate()) {
+        this.$router.push({ name: 'Team' });
+      }
     },
   },
 });
 </script>
+
+<style scoped>
+.login-view_content {
+  margin-top: -8%;
+}
+</style>
