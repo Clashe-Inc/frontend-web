a76ac6da06b0f3cb97b43426b3555b858807e2b7
Walber Oliveira <walberjr.oliveira@gmail.com>
2022-12-05 15:50:05 -0300

    #13 - Criado componente para apresentar messages de sucesso e erros

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <template lang="pug">
 VApp
+  SnackbarMessage
   VExpandXTransition
     RouterView
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import SnackbarMessage from '@/components/SnackbarMessage.vue';
 
 export default Vue.extend({
   name: 'App',
+  components: {
+    SnackbarMessage,
+  },
 });
 </script>
